542337839a4e1ca8b875d25ab6f8cdec0a81459d
Rajat Gupta <rajatgupta@Rajats-MacBook-Air.local>
2021-11-07 11:32:03 +0530

    GetMethods

--- src/com/rajat/hibernate/runners/RetrieveObjectsFromHibernate.java
+++ src/com/rajat/hibernate/runners/RetrieveObjectsFromHibernate.java
@@ -1,5 +1,43 @@
 package com.rajat.hibernate.runners;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.rajat.hibernate.entities.Student;
+
 public class RetrieveObjectsFromHibernate {
+
+	public static void main(String[] args) {
+		// Create session Factory
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		System.out.println("Factory Created  : " + factory);
+
+		// Create the session
+
+		Session currentSession = factory.getCurrentSession();
+
+		try {
+			// . Begin the transaction , Use the get mthod to get the student Details and
+			// store in object
+
+			currentSession.beginTransaction();
+
+			Student myStudent = currentSession.get(Student.class, 1);
+			System.out.println(myStudent.toString());
+
+			// Commit the transactions
+
+			currentSession.getTransaction().commit();
+
+			System.out.println("Fetched Students");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Got exception while creating student...");
+		}
+	}
 
 }
